dcbed9176c9dd76afea7b8a10c320140c254057b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-31 16:56:35 +0100

    Update UpdateReverseProxyIPList.sh

--- providerscripts/webserver/configuration/reverseproxy/nginx/UpdateReverseProxyIPList.sh
+++ providerscripts/webserver/configuration/reverseproxy/nginx/UpdateReverseProxyIPList.sh
@@ -1,1 +1,24 @@
-
+#!/bin/sh
+#####################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: This will update the current list of active webserver IP addresses 
+# in a reverse proxy machine. IP addresses can either be added or removed based
+# on whether a new webserver has been provisioned or an old webserver has been shutdown
+# or terminated. 
+#####################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################################
+######################################################################################
+#set -x
