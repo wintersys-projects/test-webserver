cbf52f5c1d8e67e3f867977e0276abfbb9624975
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-04 23:02:43 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,6 +1,57 @@
 #!/bin/sh
 
 
+#Get ourselves orientated so we know where our home is
+USER_HOME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
+/bin/echo 'export HOME="/home/'${USER_HOME}'"' >> /home/${USER_HOME}/.bashrc
+/bin/chmod 644 /home/${USER_HOME}/.bashrc
+/bin/chown ${USER_HOME}:root /home/${USER_HOME}/.bashrc
+/bin/echo "set mouse=r" > /root/.vimrc
+
+SERVER_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
+WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+
+#Set the intialial permissions for the build
+/usr/bin/find ${HOME} -not -path '*/\.*' -type d -print0 | xargs -0 chmod 0755 # for directories
+/usr/bin/find ${HOME} -not -path '*/\.*' -type f -print0 | xargs -0 chmod 0500 # for files
+/bin/chown ${SERVER_USER}:root ${HOME}/.ssh
+/bin/chmod 750 ${HOME}/.ssh
+
+/bin/echo 'export HOME=`/bin/cat /home/homedir.dat` && /bin/sh ${1} ${2} ${3} ${4} ${5} ${6}' > /usr/bin/run
+/bin/chown ${SERVER_USER}:root /usr/bin/run
+/bin/chmod 750 /usr/bin/run
+/bin/echo 'export HOME=`/bin/cat /home/homedir.dat` && /usr/bin/run ${HOME}/application/configuration/ApplicationConfigurationUpdate.sh' > /usr/bin/config
+/bin/chown ${SERVER_USER}:root /usr/bin/run
+/bin/chmod 750 /usr/bin/config
+
+#Set up more operational directories
+if ( [ ! -d ${HOME}/.ssh ] )
+then
+    /bin/mkdir ${HOME}/.ssh
+fi
+
+if ( [ ! -d ${HOME}/runtime ] )
+then
+    /bin/mkdir ${HOME}/runtime
+    /bin/chown ${SERVER_USER}:${SERVER_USER} ${HOME}/runtime
+    /bin/chmod 755 ${HOME}/runtime
+fi
+
+#Setup operational directories if needed
+if ( [ ! -d ${HOME}/logs/initialbuild ] )
+then
+    /bin/mkdir -p ${HOME}/logs/initialbuild
+fi
+
+if ( [ ! -d ${HOME}/super ] )
+then
+    /bin/mkdir ${HOME}/super
+fi
+
+/bin/mv ${HOME}/utilities/security/Super.sh ${HOME}/super
+/bin/chmod 400 ${HOME}/super/Super.sh
+
+
 /bin/echo "${0} Installing Datastore tools"
 ${HOME}/providerscripts/datastore/InitialiseDatastoreConfig.sh
 ${HOME}/providerscripts/datastore/InitialiseAdditionalDatastoreConfigs.sh
