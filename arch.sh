f3a3c14a71d415a94658eab35cc3e9057f2cd3cb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-04 20:39:14 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 
+
+/bin/echo "${0} Installing Datastore tools"
+${HOME}/providerscripts/datastore/InitialiseDatastoreConfig.sh
+${HOME}/providerscripts/datastore/InitialiseAdditionalDatastoreConfigs.sh
+
