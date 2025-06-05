b132a455260dd996e88786a0ef3d22506f528a4b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-05 18:42:23 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -71,10 +71,11 @@ then
     
     /bin/echo "${0} Installing the bespoke application"
     ${HOME}/application/InstallApplication.sh &
+    ${HOME}/installscripts/InstallReverseProxy.sh
+else
+    /bin/echo "${0} Installing/Refreshing webserver "
+    ${HOME}/installscripts/InstallWebserver.sh &
 fi
-
-/bin/echo "${0} Installing/Refreshing webserver "
-${HOME}/installscripts/InstallWebserver.sh &
 
 /bin/echo "${0} Initialising crontab"
 ${HOME}/cron/InitialiseCron.sh
