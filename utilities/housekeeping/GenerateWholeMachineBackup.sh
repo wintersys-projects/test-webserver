7ac5e00fc76700da2378155921c2072e838507c0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-08 22:55:44 +0100

    Update GenerateWholeMachineBackup.sh

--- utilities/housekeeping/GenerateWholeMachineBackup.sh
+++ utilities/housekeeping/GenerateWholeMachineBackup.sh
@@ -22,7 +22,7 @@ do
   count="`/usr/bin/expr ${count} + 1`"
   cd ${HOME}/runtime  
   /usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_runtime.tar.gz . 
-  /usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_backup.tar.gz --exclude="${archive_name}_backup.tar.gz" --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / 
+  /usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_backup.tar.gz --exclude="${archive_name}_backup.tar.gz" --exclude='/var/www/html/*' --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / 
 done
 
 if ( [ "${count}" = "5" ] )
