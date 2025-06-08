4be45406e9a46220e1e5be090d60b6cc63816cbc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-08 14:09:27 +0100

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -188,38 +188,38 @@ ${HOME}/utilities/security/EnforcePermissions.sh
 ${HOME}/providerscripts/webserver/RestartWebserver.sh
 
 /bin/echo "${0} Updating Software"
-if ( [ "${GENERATE_WHOLE_MACHINE_DUMP}" = "0" ] )
-then
+#if ( [ "${GENERATE_WHOLE_MACHINE_DUMP}" = "0" ] )
+#then
 	${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS} &
-elif ( [ "${GENERATE_WHOLE_MACHINE_DUMP}" = "1" ] )
-then
-	${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
- 	/bin/echo "${0} Generating Whole Machine Backup"
-
-	if ( [ ! -d ${HOME}/machinedump ] )
- 	then
-  		/bin/mkdir ${HOME}/machinedump
-	fi
-
-      	if ( [ "`/usr/bin/hostname | /bin/grep '^ws-'`" != "" ] )
-	then
- 		archive_name="webserver"
-	fi
-
- 	count="1"
-	/bin/ls /tmp/dusty.$$
- 
-	while ( [ "$?" != "0" ] && [ "${count}" -lt "5" ] )
- 	do	
-  		count="`/usr/bin/expr ${count} + 1`"
-        	cd ${HOME}/runtime
-        	/usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_runtime.tar.gz . 
-		/usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_backup.tar.gz --exclude="${archive_name}_backup.tar.gz" --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / 
-	done
-
- 	if ( [ "${count}" = "5" ] )
-  	then
-		${HOME}/providerscripts/email/SendEmail.sh "FAILED TO GENERATE WHOLE MACHINE BACKUP on the webserver" "It hasn't been possible to generate a whole machine backup on the webserverr machine" "ERROR"
-   	fi
-
-fi
+#elif ( [ "${GENERATE_WHOLE_MACHINE_DUMP}" = "1" ] )
+#then#
+#	${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
+# 	/bin/echo "${0} Generating Whole Machine Backup"###
+#
+#	if ( [ ! -d ${HOME}/machinedump ] )
+ #	then
+  #		/bin/mkdir ${HOME}/machinedump
+#	fi
+#
+ #     	if ( [ "`/usr/bin/hostname | /bin/grep '^ws-'`" != "" ] )
+#	then
+ #		archive_name="webserver"
+#	fi
+#
+ #	count="1"
+#	/bin/ls /tmp/dusty.$$
+ #
+#	while ( [ "$?" != "0" ] && [ "${count}" -lt "5" ] )
+ #	do	
+  #		count="`/usr/bin/expr ${count} + 1`"
+   #     	cd ${HOME}/runtime
+    #    	/usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_runtime.tar.gz . 
+	#	/usr/bin/tar -cvpzf ${HOME}/machinedump/${archive_name}_backup.tar.gz --exclude="${archive_name}_backup.tar.gz" --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / #
+#	done
+#
+# 	if ( [ "${count}" = "5" ] )
+#  	then#
+#		${HOME}/providerscripts/email/SendEmail.sh "FAILED TO GENERATE WHOLE MACHINE BACKUP on the webserver" "It hasn't been possible to generate a whole machine backup on the webserverr machine" "ERROR"
+ #  	fi
+#
+#fi
