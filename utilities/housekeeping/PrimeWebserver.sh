9657149ae8da8a36a7c928dbe9c34dad62ea7e13
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-09 21:13:12 +0100

    Update PrimeWebserver.sh

--- utilities/housekeeping/PrimeWebserver.sh
+++ utilities/housekeeping/PrimeWebserver.sh
@@ -1,10 +1,20 @@
 HOME="`/bin/cat /home/homedir.dat`"
+WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 
 /bin/rm ${HOME}/runtime/FIREWALL-ACTIVE
 
 USER_NAME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
 
 original_user="`/bin/ls -l /home | /bin/grep "X*X" | /usr/bin/awk '{print $NF}' | /bin/grep -v "${USER_NAME}"`"
+
+/bin/mv /home/${original_user}/ssl/live/${WEBSITE_URL}/fullchain.pem ${HOME}/ssl/live/${WEBSITE_URL}
+/bin/mv /home/${original_user}/ssl/live/${WEBSITE_URL}/privkey.pem ${HOME}/ssl/live/${WEBSITE_URL}
+
+/bin/chown www-data:www-data ${HOME}/ssl/live/${WEBSITE_URL}/fullchain.pem
+/bin/chown www-data:www-data ${HOME}/ssl/live/${WEBSITE_URL}/privkey.pem
+
+/bin/chmod 400 ${HOME}/ssl/live/${WEBSITE_URL}/fullchain.pem
+/bin/chmod 400 ${HOME}/ssl/live/${WEBSITE_URL}/privkey.pem
 
 webserver_config="`/bin/cat ${HOME}/runtime/WEBSERVER_CONFIG_LOCATION.dat`"
 
