47293dc49efdd55cb1f7a9fbb536a37e764a51eb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-12 08:52:15 +0100

    Update GenerateWholeMachineBackup.sh

--- utilities/housekeeping/GenerateWholeMachineBackup.sh
+++ utilities/housekeeping/GenerateWholeMachineBackup.sh
@@ -21,8 +21,10 @@ while ( [ "$?" != "0" ] && [ "${count}" -lt "5" ] )
 do
   count="`/usr/bin/expr ${count} + 1`"
   cd ${HOME}/runtime  
-  /usr/bin/tar -cvpf ${HOME}/machinedump/${archive_name}_runtime.tar . 
-  /usr/bin/tar -cvpf ${HOME}/machinedump/${archive_name}_backup.tar --exclude="${HOME}/machinedump/*" --exclude='/var/www/html/*' --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / 
+  /usr/bin/tar -cpf ${HOME}/machinedump/${archive_name}_runtime.tar . 
+  /usr/bin/tar cpf ${HOME}/machinedump/${archive_name}_hidden.tar `/usr/bin/find / -type f -name '.*' -not -path "${HOME}/machinedump/*" -not -path "/var/www/html/*" -not -path '/dev/*' -not -path '/proc/*' -not -path  '/sys/*' -not -path '/tmp/*' -not -path '/run/*' -not -path '/mnt/*' -not -path '/media/*' -not -path '/lost+found/*'`
+  /usr/bin/tar -cpf ${HOME}/machinedump/${archive_name}_backup.tar --exclude="${HOME}/machinedump/*" --exclude='/var/www/html/*' --exclude='dev/*' --exclude='proc/*' --exclude='sys/*' --exclude='tmp/*' --exclude='run/*' --exclude='mnt/*' --exclude='media/*' --exclude='lost+found/*' / 
+
 done
 
 if ( [ "${count}" = "5" ] )
