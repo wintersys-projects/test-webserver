5df7b27a746949b29b84deb8f4e76adbbcb12b61
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-12 21:17:50 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -19,16 +19,6 @@
 #######################################################################################################
 #set -x
 
-#Get ourselves orientated so we know where our home is
-USER_HOME="`/usr/bin/awk -F: '{ print $1}' /etc/passwd | /bin/grep "X*X"`"
-/bin/echo 'export HOME="/home/'${USER_HOME}'"' >> /home/${USER_HOME}/.bashrc
-/bin/chmod 644 /home/${USER_HOME}/.bashrc
-/bin/chown ${USER_HOME}:root /home/${USER_HOME}/.bashrc
-
-SERVER_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
-APPLICATION_LANGUAGE="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATIONLANGUAGE'`"
-WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
-
 if ( [ "`/usr/bin/hostname | /bin/grep '^auth-'`" != "" ] )
 then
         ${HOME}/utilities/config/StoreConfigValue.sh "WEBSITEURLORIGINAL" "${WEBSITE_URL}"
@@ -36,81 +26,14 @@ then
         ${HOME}/utilities/config/StoreConfigValue.sh "WEBSITEURL" "${WEBSITE_URL}"
 fi
 
-/bin/echo "set mouse=r" > /root/.vimrc
-
-#Set the intialial permissions for the build
-/usr/bin/find ${HOME} -not -path '*/\.*' -type d -print0 | xargs -0 chmod 0755 # for directories
-/usr/bin/find ${HOME} -not -path '*/\.*' -type f -print0 | xargs -0 chmod 0500 # for files
-/bin/chown ${SERVER_USER}:root ${HOME}/.ssh
-/bin/chmod 750 ${HOME}/.ssh
-
-/bin/echo 'export HOME=`/bin/cat /home/homedir.dat` && /bin/sh ${1} ${2} ${3} ${4} ${5} ${6}' > /usr/bin/run
-/bin/chown ${SERVER_USER}:root /usr/bin/run
-/bin/chmod 750 /usr/bin/run
-/bin/echo 'export HOME=`/bin/cat /home/homedir.dat` && /usr/bin/run ${HOME}/application/configuration/ApplicationConfigurationUpdate.sh' > /usr/bin/config
-/bin/chown ${SERVER_USER}:root /usr/bin/config
-/bin/chmod 750 /usr/bin/config
-/bin/echo 'export HOME=`/bin/cat /home/homedir.dat` && /bin/touch ${HOME}/runtime/CONFIG_BEING_CHANGED' > /usr/bin/changing_config
-/bin/chown ${SERVER_USER}:root /usr/bin/changing_config
-/bin/chmod 750 /usr/bin/changing_config
-/bin/echo 'export HOME=`/bin/cat /home/homedir.dat` && /bin/rm ${HOME}/runtime/CONFIG_BEING_CHANGED' > /usr/bin/changed_config
-/bin/chown ${SERVER_USER}:root /usr/bin/changed_config
-/bin/chmod 750 /usr/bin/changed_config
-
-#Set up more operational directories
-if ( [ ! -d ${HOME}/.ssh ] )
-then
-        /bin/mkdir ${HOME}/.ssh
-fi
-
-if ( [ ! -d ${HOME}/runtime ] )
-then
-        /bin/mkdir ${HOME}/runtime
-        /bin/chown ${SERVER_USER}:${SERVER_USER} ${HOME}/runtime
-        /bin/chmod 755 ${HOME}/runtime
-fi
-
-#Setup operational directories if needed
-if ( [ ! -d ${HOME}/logs/initialbuild ] )
-then
-        /bin/mkdir -p ${HOME}/logs/initialbuild
-fi
-
-if ( [ ! -d ${HOME}/super ] )
-then
-        /bin/mkdir ${HOME}/super
-fi
-
-/bin/mv ${HOME}/utilities/security/Super.sh ${HOME}/super
-/bin/chmod 400 ${HOME}/super/Super.sh
-
-out_file="initialbuild/webserver-build-out-`/bin/date | /bin/sed 's/ //g'`"
-exec 1>>${HOME}/logs/${out_file}
-err_file="initialbuild/webserver-build-err-`/bin/date | /bin/sed 's/ //g'`"
-exec 2>>${HOME}/logs/${err_file}
-
-cd ${HOME}
-
-#/bin/echo "${0} Installing Datastore tools"
-#${HOME}/providerscripts/datastore/InitialiseDatastoreConfig.sh
-#${HOME}/providerscripts/datastore/InitialiseAdditionalDatastoreConfigs.sh
-
-cd ${HOME}
+/bin/rm -r ${HOME}/logs/*
 
 if ( [ -d /var/www/html ] )
 then
   /bin/rm -r /var/www/html/*
 fi
 
-/bin/echo "${0} Installing the bespoke application"
-${HOME}/application/InstallApplication.sh
-
-
 if ( [ "${APPLICATION_LANGUAGE}" = "PHP" ] )
 then
         /bin/mkdir /run/php
 fi
-
-/bin/echo "${0} Initialising crontab"
-${HOME}/cron/InitialiseCron.sh
-
