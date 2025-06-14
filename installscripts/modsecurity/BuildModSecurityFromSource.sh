6a81e96a5daeaceb16f3bf85fe74acb0e973cd0a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-14 20:56:23 +0100

    Update BuildModSecurityFromSource.sh

--- installscripts/modsecurity/BuildModSecurityFromSource.sh
+++ installscripts/modsecurity/BuildModSecurityFromSource.sh
@@ -1,14 +1,9 @@
-apt-get install bison build-essential ca-certificates curl dh-autoreconf doxygen flex gawk git iputils-ping libcurl4-gnutls-dev libexpat1-dev libgeoip-dev liblmdb-dev libpcre3-dev libpcre++-dev libssl-dev libtool libxml2 libxml2-dev libyajl-dev locales lua5.3-dev pkg-config wget zlib1g-dev zlibc libxslt libgd-dev
-
-
-cd /opt 
-/usr/bin/git clone https://github.com/SpiderLabs/ModSecurity
-cd ModSecurity
-usr/bin/git submodule init
+apt-get install git g++ apt-utils autoconf automake build-essential libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre2-dev libtool libxml2-dev libyajl-dev pkgconf zlib1g-dev
+/usr/bin/git clone https://github.com/owasp-modsecurity/ModSecurity
+cd ModSecurity/
+/usr/bin/git submodule init
 /usr/bin/git submodule update
-./build.sh 
-./configure
-/usr/bin/make
-/usr/bin/make install
-cd /opt 
-/usr/bin/git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git
+/bin/sh build.sh
+./configure --with-pcre2
+make
+make install
