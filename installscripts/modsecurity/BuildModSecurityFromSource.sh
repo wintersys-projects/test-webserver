805ec74ce3a40c9aff60a12672cf11cbc8b2a75c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-14 21:24:24 +0100

    Update BuildModSecurityFromSource.sh

--- installscripts/modsecurity/BuildModSecurityFromSource.sh
+++ installscripts/modsecurity/BuildModSecurityFromSource.sh
@@ -1,4 +1,6 @@
 apt-get install git g++ apt-utils autoconf automake build-essential libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre2-dev libtool libxml2-dev libyajl-dev pkgconf zlib1g-dev
+
+cd /opt
 /usr/bin/git clone https://github.com/owasp-modsecurity/ModSecurity
 cd ModSecurity/
 /usr/bin/git submodule init
@@ -7,3 +9,15 @@ cd ModSecurity/
 ./configure --with-pcre2
 make
 make install
+
+cd /opt
+/usr/bin/git clone https://github.com/owasp-modsecurity/ModSecurity-nginx.git
+
+Add this to the nginx build from source part when building nginx from source
+./configure --add-module=/opt/ModSecurity-nginx
+
+BuildModSecurity (generic for all webserver types)
+1. Compile libmodsecurity as above
+For nginx clone modsecurity-nginx  as above
+2. Clone ModSecurity-nginx as above
+3. compile nginx from source with mod-security added to the configure command
