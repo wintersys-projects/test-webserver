21cb8c68748b0d919dc993cfb4a24afb833ca6f9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-14 23:29:50 +0100

    Update BuildModSecurityFromSource.sh

--- installscripts/modsecurity/BuildModSecurityFromSource.sh
+++ installscripts/modsecurity/BuildModSecurityFromSource.sh
@@ -1,5 +1,3 @@
-apt-get install git g++ apt-utils autoconf automake build-essential libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre2-dev libtool libxml2-dev libyajl-dev pkgconf zlib1g-dev
-
 cd /opt
 /usr/bin/git clone https://github.com/owasp-modsecurity/ModSecurity
 cd ModSecurity/
