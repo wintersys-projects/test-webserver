bc024caf1e72857d4e238810b92b927d6ac7a8ef
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-14 23:57:33 +0100

    Update BuildModSecurityFromSource.sh

--- installscripts/modsecurity/BuildModSecurityFromSource.sh
+++ installscripts/modsecurity/BuildModSecurityFromSource.sh
@@ -13,3 +13,20 @@ cd /opt
 
 
 
+/bin/rm -rf /usr/share/modsecurity-crs
+/usr/bin/git clone https://github.com/coreruleset/coreruleset /usr/local/modsecurity-crs
+/bin/mv /usr/local/modsecurity-crs/crs-setup.conf.example /usr/local/modsecurity-crs/crs-setup.conf
+/bin/mv /usr/local/modsecurity-crs/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf.example /usr/local/modsecurity-crs/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf
+/bin/mkdir -p /etc/nginx/modsec
+/bin/cp /opt/ModSecurity/unicode.mapping /etc/nginx/modsec
+/bin/cp /opt/ModSecurity/modsecurity.conf-recommended /etc/nginx/modsec
+/bin/cp /etc/nginx/modsec/modsecurity.conf-recommended /etc/nginx/modsec/modsecurity.conf
+
+/bin/sed -i 's/^SecRuleEngine.*/SecRuleEngine On/' /etc/nginx/modsec/modsecurity.conf
+
+/bin/echo "Include /etc/nginx/modsec/modsecurity.conf" > /etc/nginx/modsec/main.conf
+/bin/echo "Include /usr/local/modsecurity-crs/crs-setup.conf" >> /etc/nginx/modsec/main.conf
+/bin/echo "Include /usr/local/modsecurity-crs/rules/*.conf" >> /etc/nginx/modsec/main.conf
+
+
+
